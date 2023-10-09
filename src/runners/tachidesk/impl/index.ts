66075dcef51a17a0eb8bcf12fa50e32f960a5fe9
mantton <mantton.official@gmail.com>
2023-10-09 18:39:27 -0400

    ref: move tachidesk to this list

---
+++ src/runners/tachidesk/impl/index.ts
@@ -0,0 +1,4 @@
+export * from "./contentSource";
+export * from "./directoryHandler";
+export * from "./pageLinkProvider";
+export * from "./setupProvider";
---
+++ src/runners/tachidesk/utils/constants.ts
@@ -0,0 +1,1 @@
+export const LITE_ROUTER = "/api/v1/lite";
