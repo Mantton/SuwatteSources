40eee770a29087a6fbae5c04ac6790c4c6fae6fe
mantton <mantton.official@gmail.com>
2024-01-09 19:22:35 -0500

    feat: add FirstKissManga

---
+++ src/tests/firstkissmanga.test.ts
@@ -0,0 +1,56 @@
+import emulate from "@suwatte/emulator";
+import { Target } from "../runners/firstkissmanga";
+import {
+  ChapterDataSchema,
+  ChapterSchema,
+  ContentSchema,
+  PagedResultSchema,
+} from "@suwatte/validate";
+describe("FirstKissManga Tests", () => {
+  const source = emulate(Target);
+
+  describe("Paged results", () => {
+    test("Popular", async () => {
+      const data = await source.getDirectory({
+        page: 1,
+        listId: "template_popular_list",
+      });
+      expect(PagedResultSchema.parse(data)).toEqual(expect.any(Object));
+      expect(data.results.length).toBeGreaterThan(1);
+    });
+    test("Query", async () => {
+      const data = await source.getDirectory({
+        page: 1,
+        query: "doctor",
+      });
+      expect(PagedResultSchema.parse(data)).toEqual(expect.any(Object));
+      expect(data.results.length).toBeGreaterThan(1);
+    });
+    test("Latest", async () => {
+      const data = await source.getDirectory({
+        page: 1,
+        listId: "template_latest_list",
+      });
+      expect(PagedResultSchema.parse(data)).toEqual(expect.any(Object));
+      expect(data.results.length).toBeGreaterThan(1);
+    });
+  });
+  const id = "/manga/gray-maid/";
+
+  test("Profile", async () => {
+    const content = await source.getContent(id);
+    expect(ContentSchema.parse(content)).toEqual(expect.any(Object));
+  });
+
+  test("Chapters", async () => {
+    const chapters = await source.getChapters(id);
+    expect(ChapterSchema.array().parse(chapters)).toEqual(expect.any(Array));
+    expect(chapters.length).toBeGreaterThan(1);
+  });
+
+  test("Reader", async () => {
+    const chapterId = "https://1st-kissmanga.net/manga/gray-maid/chapter-2/";
+    const data = await source.getChapterData(id, chapterId);
+    expect(ChapterDataSchema.parse(data)).toEqual(expect.any(Object));
+  });
+});
