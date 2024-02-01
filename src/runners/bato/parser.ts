5eaf641ed37bf86d695fbefd9eb8449d04c92ab4
childryad <chill.dryad@gmail.com>
2024-01-31 19:13:11 -0500

    wrote the right code in the right repo this time

--- src/runners/bato/parser.ts
+++ src/runners/bato/parser.ts
@@ -256,15 +256,7 @@ export class Parser {
       enc.Utf8
     );
 
-    if (!imgAccListString) throw new Error("Invalid Acc List String");
-
     const imgAccList: string[] = JSON.parse(imgAccListString);
-
-    console.info(imgAccList)
-
-    // if (!Array.isArray(imgAccList) || imgAccList.length == 0)
-    //   throw new Error("Acc List Is Invalid");
-
     const urls = imgHttpList.map((v, i) => `${v}?${imgAccList[i]}`);
 
     return urls.map((url) => ({ url }));
