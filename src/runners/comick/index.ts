00d6faf11ed1037d69bf8aa26f8e27925a062b79
Jishnu Sen <jishnu1@gmail.com>
2024-04-13 03:37:55 -0700

    bump version

--- src/runners/comick/index.ts
+++ src/runners/comick/index.ts
@@ -45,7 +45,7 @@ export class Target
   info: RunnerInfo = {
     id: "app.comick",
     name: "ComicK",
-    version: 0.54,
+    version: 0.55,
     website: "https://comick.io/home",
     supportedLanguages: [],
     thumbnail: "comick.png",
